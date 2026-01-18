61edd42db86e90e10ac2f510a31fb14e278c4122
Varad203 <varadshamghuge@gmail.com>
2026-01-18 23:52:32 +0530

    Create Task model with schema definition

---
+++ models/Task.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  status: {
+    type: String,
+    enum: ["todo", "in-progress", "completed"],
+    default: "todo"
+  },
+  userId: mongoose.Schema.Types.ObjectId,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
